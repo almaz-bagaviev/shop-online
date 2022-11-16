edebcb6608cedde0674fec346f130a58504749e7
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-11-16 16:35:40 +0300

    change xaml

--- ShopOnline/MainWindow.xaml.cs
+++ ShopOnline/MainWindow.xaml.cs
@@ -1,19 +1,17 @@
-﻿namespace ShopOnline;
+﻿using System.Windows;
+using ShopOnline.Models;
+
+namespace ShopOnline;
 
 public partial class MainWindow : Window
 {
-    Model model;
-    
+    ViewModel model;
+
     public MainWindow()
     {
         InitializeComponent();
-        model = new(mainDataGrid);
-        model.MainSQL();
-    }
 
-    private void DataGrid_CurrentCellChanged(object sender, EventArgs e) => model.CurrentCellChanged();
-    private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e) => model.CellEditEnding();
-    private void MenuItemAddClick(object sender, RoutedEventArgs e) => model.MenuItemAdd(sender, e);
-    private void MenuItemRemoveClick(object sender, RoutedEventArgs e) => model.Remove();
-    private void FullTable_Click(object sender, RoutedEventArgs e) => model.FullTable();
+        model = new();
+        OrdersListBox.ItemsSource = model.Orders;
+    }
 }
