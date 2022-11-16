e65bdd931b55243afe2e8d2558569b1a1c446523
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-11-16 16:34:25 +0300

    change request

--- ShopOnline/SqlRequests.sql
+++ ShopOnline/SqlRequests.sql
@@ -1,58 +1,75 @@
-﻿CREATE TABLE [dbo].[Clients]
+﻿CREATE TABLE [dbo].[Client]
 (
-	[Id] INT  IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	[LastName] NVARCHAR(50) NOT NULL,
-	[FirstName] NVARCHAR(50) NOT NULL,
-	[MiddleName] NVARCHAR(50) NOT NULL,
-	[Phone] NVARCHAR(50),
-	[Email] NVARCHAR(50) NOT NULL
-)
+    [Id] INT IDENTITY (1, 1) PRIMARY KEY NOT NULL,
+    [LastName] NVARCHAR (50) NOT NULL,
+    [FirstName] NVARCHAR (50) NOT NULL,
+    [MiddleName] NVARCHAR (50) NOT NULL
+);
 
-INSERT INTO Clients VALUES (N'Агапов', N'Сергей', N'Петрович', '555-77-88', 'agapov@mail.ru'),
-(N'Александров', N'Пётр', N'Александрович', '222-87-54', 'alexandrov@gmail.com'),
-(N'Бушенёв', N'Родион', N'Максимович', '542-98-75', 'byshenev@list.ru'),
-(N'Балашёв', N'Максим', N'Сергеевич', '457-00-98', 'balashov@mail.ru'),
-(N'Беляев', N'Сергей', N'Андреевич', '222-78-96', 'belyev@gmail.com'),
-(N'Бородин', N'Константин', N'Алексеевич', '478-99-36', 'borodin@mail.ru'),
-(N'Васильева', N'Анна', N'Ивановна', '222-22-22', 'vasileva@mail.ru'),
-(N'Вишневский', N'Виктор', N'Матвеевич', '214-96-32', 'vishnevskiy@gmail.com'),
-(N'Вагапов', N'Булат', N'Михаилович', '555-45-77', 'vagapov@mail.ru'),
-(N'Владимиров', N'Эдуард', N'Романович', '555-77-88', 'vladimirov@gmail.com'),
-(N'Локотунина', N'Екатерина', N'Константиновна', '478-66-25', 'lokotunina@gmail.com');
-
-SELECT * FROM Clients;
-
---UPDATE Clients SET FirstName = N'Виталий'
---WHERE LastName = N'Бородин';
-
---UPDATE Clients SET Phone = '777-98-01'
---WHERE Id = 2;
-
---SELECT * FROM Clients;
-
-CREATE TABLE [dbo].[Purchases]
+CREATE TABLE [dbo].[Contacts]
 (
-	[Id] INT IDENTITY(1, 1) PRIMARY KEY,
-	[ItemCode] INT NOT NULL,
-	[Product] NVARCHAR(50) NOT NULL	
-)
+    [Id] INT IDENTITY (1, 1) PRIMARY KEY NOT NULL,
+    [Phone] NVARCHAR (20) NOT NULL,
+    [Email] NVARCHAR (100) NOT NULL,
+    [ClientId] INT NOT NULL
+);
 
-INSERT INTO Purchases VALUES(34, N'Ноутбук HP'),
-(67, N'Видеокарта Palit GeForce 1660'),
-(11, N'Наушники Sony'),
-(34, N'Холодильник Samsung'),
-(44, N'Клавиатура HyperX'),
-(10, N'Смартфон Iphone 13 mini'),
-(91, N'Оперативная память Crucial 8 ГБ'),
-(76, N'МФУ Xerox 3015'),
-(57, N'Процессор AMD Ryzen 5 2600'),
-(22, N'Кулер DeepCool GAMMAXX 400');
+CREATE TABLE [dbo].[Location]
+(
+    [Id] INT IDENTITY (1, 1) PRIMARY KEY NOT NULL,
+    [Country] NVARCHAR (50) NOT NULL,
+    [City] NVARCHAR (50) NOT NULL,
+    [ClientId] INT NOT NULL
+);
 
-SELECT * FROM Purchases;
+CREATE TABLE [dbo].[Order]
+(
+    [Id] INT IDENTITY (1, 1) PRIMARY KEY NOT NULL,
+    [Number] INT NOT NULL,
+    [Date] DATETIME NOT NULL,
+    [ClientId] INT NOT NULL,
+    [Product] NVARCHAR (MAX) NOT NULL,
+    [Count] DECIMAL (18, 2) NOT NULL
+);
 
 
-SELECT Clients.LastName, Clients.FirstName, Clients.MiddleName, Clients.Phone, Clients.Email,
-Purchases.ItemCode, Purchases.Product
-FROM Clients
-JOIN Purchases
-ON Clients.Id = Purchases.Id;
+insert into Client values(N'Абрамов', N'Александр', N'Иванович'),
+(N'Петров', N'Константин', N'Маркович'),
+(N'Локотунина', N'Екатерина', N'Константиновна'),
+(N'Ямалов', N'Кирилл', N'Львович')
+
+select * from Client;
+
+
+insert into [Location] values (N'Россия', N'Москва', 1),
+(N'Россия', N'Санкт-Петербург', 2),
+(N'Россия', N'Сочи', 3),
+(N'Белоруссия', N'Минск', 4)
+
+select * from [Location];
+
+
+insert into Contacts values('555-87-85', 'abramov@mail.ru', 1),
+('222-87-01', 'petrov@gmail.ru', 2),
+('666-97-21', 'lok@gmail.ru', 3),
+('210-74-36', 'yamalov@mail.ru', 4)
+
+select * from Contacts;
+
+
+insert into [Order] values (501, '2022-07-01 09:02:20', 1, N'Сыр', 0.5),
+(901, GETDATE(), 2, N'Рыба', 7),
+(908, GETDATE(), 3, N'Мясо', 16.7),
+(700, GETDATE(), 4, N'Хлебобулочные изделия', 0.5)
+
+select * from [Order];
+
+
+select c.LastName, c.FirstName, c.MiddleName,
+l.Country, l.City,
+ct.Phone, ct.Email,
+o.Number, o.[Date], o.Product, o.[Count]
+from Client c
+left join [Location] l on c.Id=l.ClientId
+left join Contacts ct on c.Id=ct.ClientId
+left join [Order] o on c.Id=o.ClientId
